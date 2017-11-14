5918ea919f8b08386d43bcee0d1957e5827acef5
gustid <gustidinulica@yahoo.com>
2017-11-14 11:19:44 +0200

    added slider

--- index.php
+++ index.php
@@ -99,11 +99,60 @@ session_start();
 
 
 <section >
-	<div class="wrapper2" align="center">
-		<img src="tennis.jpg" alt="">
+	<div class="slideshow-container">
 
-	</div>
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="tennis.jpg" style="width:100%">
+  <div class="text-slideshow">People watching the matches on Court 1</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="tennis2.jpg" style="width:100%">
+  <div class="text-slideshow">Rafael Nadal capturing his 10th title last year after amazing battle with Wawrinka</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="tennis3.jpg" style="width:100%">
+  <div class="text-slideshow">Ryan Harrison and Michael Venus qualify for their first final here in Timisoara</div>
+</div>
+
+</div>
+
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span>
+  <span class="dot"></span>
+  <span class="dot"></span>
+</div>
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+</script>
+
 </section>
+
 
 <footer>
 	<div class="wrapper">
